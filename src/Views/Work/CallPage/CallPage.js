46016f685486dcd614cca615286630ba8dd56151
Tushar Balar <balar.tushar1@gmail.com>
2020-11-15 18:02:00 +0530

    Fixed rebase issue

--- src/Views/Work/CallPage/CallPage.js
+++ src/Views/Work/CallPage/CallPage.js
@@ -1,27 +1,34 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { Grid, InputBase } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import { Grid, InputBase, Avatar, Divider } from "@material-ui/core";
+import Rating from "@material-ui/lab/Rating";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import MuiDialogContent from "@material-ui/core/DialogContent";
 import SendIcon from "@material-ui/icons/Send";
 import MicOffIcon from "@material-ui/icons/MicOff";
 import MicIcon from "@material-ui/icons/Mic";
 import VideocamOffIcon from "@material-ui/icons/VideocamOff";
 import VideocamIcon from "@material-ui/icons/Videocam";
 import DesktopWindowsIcon from "@material-ui/icons/DesktopWindows";
-import { connect, createLocalTracks } from "twilio-video";
 import CallEndIcon from "@material-ui/icons/CallEnd";
-import { useTranslation } from "react-i18next";
+import TypographyComponent from "../../../Components/Typography/Typography";
 import ArrowIcon from "../../../images/select_arrow.svg";
+import DialogComponent from "../../../Components/Dialog/Dialog";
+import { useTranslation } from "react-i18next";
 import { themes } from "../../../themes";
 import { useSidebar } from "../../../Provider/SidebarProvider";
+import NextArrow from "../../../images/next_arrow_white.svg";
 import { get, add } from "../../../Services/Auth.service";
 import Spinner from "../../../Components/Spinner/Spinner";
-import { SessionContext } from "../../../Provider/Provider";
+import { connect, createLocalTracks } from "twilio-video";
 import ConfirmPopupForLeavingCall from "./ConfirmPopup/ConfirmPopup";
-import Sidebar from "./Sidebar/Sidebar";
-
+import { SessionContext } from "../../../Provider/Provider";
 import "./callpage.css";
-
 const useSession = () => React.useContext(SessionContext);
+const DialogContent = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiDialogContent);
 
 const useStyles = makeStyles((theme) => ({
   search: {
@@ -62,40 +69,35 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const CallPage = (props) => {
-  const { state = {} } = props && props.location;
-  const bookingId = state.record && state.record.id_booking;
   const classes = useStyles();
   const { t } = useTranslation();
   const { user } = useSession();
-  const [counter, setCounter] = useState(5);
+  const [counter, setCounter] = React.useState(5);
   const [simpathy, setSimpathy] = useState();
   const [service_quality, setServiceQuality] = useState();
   const { setSidebarContent, setSidebar } = useSidebar();
+  const [endCallOpen, setEndCallOpen] = useState(false);
+  const { state = {} } = props && props.location;
   const [room, setRoom] = useState(null);
   const [isLoading, setLoading] = useState(false);
   const [audio, setAudio] = useState(true);
   const [onVideo, setVideo] = useState(true);
   const [isOpenConfirmPopup, openConfirmPopup] = useState(false);
-  const { record = {} } = state;
-
+  const bookingId = state.record && state.record.id_booking;
   let [userData, setUserData] = useState({
     first_name: "",
     last_name: "",
     image: "",
   });
-
-  const onSendMessage = (e) => {
+  const onSearch = (e) => {
     e.preventDefault();
   };
-
   useEffect(() => {
-    const getProfileInfo = async () => {
+    async function getData() {
       setLoading(true);
-      const res = await get(`/profile/${record.provider_id_user}`).catch(
-        (err) => {
-          setLoading(false);
-        }
-      );
+      const res = await get(`/profile/${user.id_user}`).catch((err) => {
+        setLoading(false);
+      });
 
       if (res) {
         setUserData({
@@ -105,9 +107,9 @@ const CallPage = (props) => {
       } else {
         setLoading(false);
       }
-    };
-    getProfileInfo();
-  }, [record.provider_id_user]);
+    }
+    getData();
+  }, [state]);
 
   useEffect(() => {
     const timer =
@@ -115,39 +117,136 @@ const CallPage = (props) => {
     return () => clearInterval(timer);
   }, [counter]);
 
+  const changeSearch = (e) => {};
+  const [dynamicCounter, setDynamicCounter] = useState();
+  const [timerId, setTimerID] = useState("");
+
+  const timer = useCallback(() => {
+    var count1 = dynamicCounter;
+    var counter1 = setInterval(timer, 1000);
+    count1 = count1 - 1;
+    if (count1 === -1) {
+      clearInterval(counter1);
+      return;
+    }
+
+    var seconds = count1 % 60;
+    var minutes = Math.floor(count1 / 60);
+    var hours = Math.floor(minutes / 60);
+    minutes %= 60;
+    hours %= 60;
+
+    document.getElementById(timerId).innerHTML =
+      hours + "hours : " + minutes + "minutes :" + seconds;
+  }, [dynamicCounter, timerId]);
+  // var counter = 5;
+
+  // setInterval(function () {
+  //   counter--;
+  //   if (counter >= 0) {
+  //     let span = document.getElementById("count");
+  //     span.innerHTML = counter;
+  //   }
+  //   if (counter === 0) {
+  //     clearInterval(counter);
+  //   }
+  // }, 1000);
+
   useEffect(() => {
     setSidebar(true);
     setSidebarContent(
       <div style={{ margin: 20 }}>
-        {isLoading ? <Spinner /> : <Sidebar userData={userData} />}
+        {isLoading ? (
+          <Spinner />
+        ) : (
+          <React.Fragment>
+            <Avatar className={classes.large} src={userData.image} />
+            <TypographyComponent
+              title={`${userData.first_name} ${userData.last_name}`}
+            />
+            <Divider className="divider" />
+            <div className="call-time">
+              <TypographyComponent title="Time left" />
+              <TypographyComponent title="120 Min" />
+            </div>
+            <div className="stay-Longer">
+              <span>Stay Longer?</span>
+              {/* <img src={ArrowIcon} alt="stay longer" /> */}
+            </div>
+            <div>
+              <span
+                id="timer-15"
+                onClick={() => {
+                  setDynamicCounter(900);
+                  setTimerID("timer-15");
+                }}
+              >
+                {!timerId && "15 min"}
+              </span>
+              <span
+                onClick={() => {
+                  setDynamicCounter(1800);
+                  setTimerID("timer-30");
+                }}
+                id="timer-30"
+              >
+                30 min
+              </span>
+              <span
+                onClick={() => {
+                  setDynamicCounter(3600);
+                  setTimerID("timer-60");
+                }}
+                id="timer-60"
+              >
+                60 min
+              </span>
+            </div>
+            <div>
+              <TypographyComponent title="Total:" />
+              <TypographyComponent title="00.0$" />
+            </div>
+          </React.Fragment>
+        )}
       </div>
     );
-  }, [setSidebar, isLoading, setSidebarContent, state, userData]);
-
-    useEffect(() => {
-    if (counter === 0) {
-      onFinish();
-    }
-  }, [onFinish, counter]);
-
+  }, [
+    setSidebarContent,
+    setSidebar,
+    classes.large,
+    timerId,
+    isLoading,
+    userData.first_name,
+    userData.last_name,
+    userData.image,
+  ]);
   const onEndCall = () => {
-    openConfirmPopup(true);
+    setEndCallOpen(true);
   };
 
-  const makeAudioTrackDisable = () => {
+  const onLeaveCall = () => {};
+
+  const onContinueCall = () => {};
+
+  const onReportAbuse = () => {};
+
+  const onLeave = () => {};
+
+  const audioTracksdisable = () => {
     room.localParticipant.audioTracks.forEach((publication) => {
       if (audio) {
         publication.track.disable();
         setAudio(false);
+        console.log("local audio disabled");
       } else {
         publication.track.enable();
         setAudio(true);
+        console.log("local audio enable");
       }
     });
   };
-
   const videoTracksdisable = () => {
-    //handle video call here
+    // handle video call here
     room.localParticipant.videoTracks.forEach((publication) => {
       if (onVideo) {
         setVideo(false);
@@ -158,126 +257,120 @@ const CallPage = (props) => {
       }
     });
   };
-  function handleTrackEnabled(track) {
-    track.on("enabled", () => {
-      alert("mute or unmute");
-    });
-  }
-  const onFinish = useCallback(() => {
-    getTwillioToken();
-  }, [bookingId, user.id_user]);
 
-  const getTwillioToken = () => {
+  const onFinish = useCallback(() => {
     const params = {
       user_id: user.id_user,
       booking_id: bookingId,
     };
+    function attachTrack(track, container) {
+      container.appendChild(track.attach());
+    }
+    function participantConnected(participant) {
+      participant.tracks.forEach((publication) => {
+        if (publication.isSubscribed) {
+          attachTrack(
+            publication.track,
+            document.getElementById("remote-media-div")
+          );
+        }
+      });
+      participant.on("trackSubscribed", (track) => {
+        attachTrack(track, document.getElementById("remote-media-div"));
+      });
+    }
+
     add("/video/join", params).then((res) => {
+      // Temporary code start - This code will execute if the auth token is invalid/expired/not-logged-in
+      console.log(res);
+
+      // Temporary code end
+
       let video_token = res.data.video_token;
+
       createLocalTracks({
         audio: true,
         video: { width: 150, height: 150 },
       }).then(function (localTracks) {
         const localMediaContainer = document.getElementById("local-media");
-
         localTracks.forEach(function (track) {
           localMediaContainer.innerHTML = "";
           localMediaContainer.appendChild(track.attach());
         });
       });
-      connectVideo(video_token);
-    });
-  }
+      connect(video_token, { booking_id: bookingId }).then(
+        (room) => {
+          console.log(`Successfully joined a Room: ${room}`);
+          setRoom(room);
+          // Log any Participants already connected to the Room
 
-  const connectVideo = (video_token) => {
-    connect(video_token, { booking_id: bookingId }).then(
-      (room) => {
-        setRoom(room);
-        room.participants.forEach(participantConnected);
+          room.participants.forEach(participantConnected);
 
-        room.participants.forEach((participant) => {
-          console.log(
-            `Participant "${participant.identity}" is connected to the Room`
-          );
-          participant.tracks.forEach((publication) => {
-            console.log("publication", publication);
-            if (publication.track) {
-              document
-                .getElementById("remote-media-div-already")
-                .appendChild(publication.track.attach());
-            }
+          room.participants.forEach((participant) => {
+            console.log(
+              `Participant "${participant.identity}" is connected to the Room`
+            );
+            participant.tracks.forEach((publication) => {
+              console.log("publication", publication);
+              if (publication.track) {
+                document
+                  .getElementById("remote-media-div-already")
+                  .appendChild(publication.track.attach());
+              }
+            });
           });
-        });
-        subscribeEventsForParticipants();
-      },
-      (error) => {
-        console.error(`Unable to connect to Room: ${error.message}`);
-      }
-    );
-  }
 
-  const attachTrack = (track, container) => {
-    container.appendChild(track.attach());
-  }
+          room.on("participantConnected", (participant) => {
+            console.log(`Participant "${participant.identity}" connected`);
 
-  const participantConnected = (participant) => {
-    participant.tracks.forEach((publication) => {
-      if (publication.isSubscribed) {
-        attachTrack(
-          publication.track,
-          document.getElementById("remote-media-div")
-        );
-      }
-    });
-    participant.on("trackSubscribed", (track) => {
-      attachTrack(track, document.getElementById("remote-media-div"));
-    });
-  }
+            participant.tracks.forEach((publication) => {
+              if (publication.isSubscribed) {
+                const track = publication.track;
+                document
+                  .getElementById("remote-media-div")
+                  .appendChild(track.attach());
+              }
+            });
 
-  const subscribeEventsForParticipants = () => {
-    room.on("participantConnected", (participant) => {
-      console.log(`Participant "${participant.identity}" connected`);
+            participant.on("trackSubscribed", (track) => {
+              document
+                .getElementById("remote-media-div")
+                .appendChild(track.attach());
+            });
+            room.participants.forEach((participant) => {
+              // participant.tracks.forEach((track) => {
+              //   document
+              //     .getElementById("remote-media-div")
+              //     .appendChild(track.attach());
+              // });
 
-      participant.tracks.forEach((publication) => {
-        if (publication.isSubscribed) {
-          const track = publication.track;
-          document
-            .getElementById("remote-media-div")
-            .appendChild(track.attach());
+              participant.on("trackAdded", (track) => {
+                document
+                  .getElementById("remote-media-div")
+                  .appendChild(track.attach());
+              });
+            });
+          });
+        },
+        (error) => {
+          console.error(`Unable to connect to Room: ${error.message}`);
         }
-      });
-
-      participant.on("trackSubscribed", (track) => {
-        document
-          .getElementById("remote-media-div")
-          .appendChild(track.attach());
-      });
-
-      room.participants.forEach((participant) => {
-        participant.tracks.forEach((track) => {
-          document
-            .getElementById("remote-media-div")
-            .appendChild(track.attach());
-        });
-
-        participant.on("trackAdded", (track) => {
-          document
-            .getElementById("remote-media-div")
-            .appendChild(track.attach());
-        });
-      });
+      );
     });
-  }
+  }, [bookingId, user.id_user]);
+
+  useEffect(() => {
+    onFinish();
+  }, [onFinish]);
 
   const handleClosePopup = () => {
     openConfirmPopup(false);
   };
-
+  
   return (
     <React.Fragment>
       <Grid container spacing={3}>
         <Grid item xs={12} md={10}>
-          <span id="count">{counter !== 0 && counter}</span>
           <div className="remoteVideoContainer">
             <div id="remote-media-div-already"></div>
             <div id="remote-media-div"></div>
@@ -288,23 +381,24 @@ const CallPage = (props) => {
           <div className="start_work_hero_left">
             <div className="service_calling_wrapper">
               <div className="service_calling_message">
-                <InputBase placeholder="Tell us write here" />
-                <span
-                  className={classes.searchIconItem}
-                  onClick={onSendMessage}
-                >
+                <InputBase
+                  placeholder="Search"
+                  inputProps={{ "aria-label": "search" }}
+                  onChange={changeSearch}
+                />
+                <span className={classes.searchIconItem} onClick={onSearch}>
                   <SendIcon className={classes.searchIcon} />
                 </span>
               </div>
               <div className="service_calling_option">
                 {audio ? (
                   <MicIcon
-                    onClick={() => makeAudioTrackDisable()}
+                    onClick={() => audioTracksdisable()}
                     className="owera_link"
                   />
                 ) : (
                   <MicOffIcon
-                    onClick={() => makeAudioTrackDisable()}
+                    onClick={() => audioTracksdisable()}
                     className="owera_link"
                   />
                 )}
@@ -319,7 +413,6 @@ const CallPage = (props) => {
                     className="owera_link"
                   />
                 )}
-
                 <DesktopWindowsIcon className="owera_link" />
                 <div
                   onClick={() => {
